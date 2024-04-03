017644ef08c399da1fdd5be2e8d11d8b1770db9e
simoneevacca <v.simone.vacca@gmail.com>
2024-04-03 12:15:10 +0200

    add call ajax (testing)

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,18 @@
       return{
         state
       }
+    },
+    mounted() {
+      state.GetContent('https://api.themoviedb.org/3/search/movie?api_key=41b23e11302057b0778ead19acd303ef&query=avatar&include_adult=false&language=it&page=1')
+      console.log();
     }
   }
 </script>
 
 <template>
   {{ state.test }}
+
+  
 </template>
 
 <style>
--- state.js
+++ state.js
@@ -1,6 +1,18 @@
 // store.js
 import { reactive } from 'vue'
+import axios from 'axios';
 
 export const state = reactive({
-  test: 'hello'
+    test: 'hello',
+
+    content: [],
+
+    GetContent(url) {
+
+        axios.get(url)
+            .then(response => {
+                this.content = response.data
+                console.log(response);
+            })
+    }
 })
