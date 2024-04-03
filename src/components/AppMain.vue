12570a53369bbc1ddbeaba565a3facf8558491cf
simoneevacca <v.simone.vacca@gmail.com>
2024-04-03 21:02:56 +0200

    add language flag

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,25 +1,28 @@
 <script>
 import { state } from '../../state.js'
-
+import LangFlag from 'vue-lang-code-flags';
 
 export default {
+    components: {
+        LangFlag,
+    },
     name: 'AppMain',
     data() {
         return {
             state,
-            search: 'avatar',           
-            
+            search: 'avatar',
+
         }
     },
 
     methods: {
-        searchContent(){
+        searchContent() {
             state.GetContent(`https://api.themoviedb.org/3/search/movie?api_key=41b23e11302057b0778ead19acd303ef&query=${this.search}&include_adult=false&language=it&page=1`)
-           
+
         }
     },
 
-    mounted() {  
+    mounted() {
     }
 }
 
@@ -30,19 +33,20 @@ export default {
     <button @click="searchContent"> cerca </button>
 
 
-   
-        <div v-for="item in state.contentElement">
-            <div class="card">
-        <div>{{ item.title }}</div>
-        <div>{{ item.original_title }}</div>
-        <div>{{ item.original_language }}</div>
-        <div>{{ item.vote_average }}</div>
+
+    <div v-for="item in state.contentElement">
+        <div class="card">
+            <div>{{ item.title }}</div>
+            <div>{{ item.original_title }}</div>
+            <lang-flag :iso="item.original_language" />
+            <div>{{ item.vote_average }}</div>
+            
+
+        </div>
+
     </div>
 
-        
-        </div>
-    
-    
+
 
 </template>
 
