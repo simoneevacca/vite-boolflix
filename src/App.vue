ab6d0469ae32735f49ca7e32e61ab867d8b95da9
simoneevacca <v.simone.vacca@gmail.com>
2024-04-03 11:21:59 +0200

    inititial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import { state } from '../state.js';
+
+
+  export default{
+    data(){
+      return{
+        state
+      }
+    }
+  }
+</script>
+
+<template>
+  {{ state.test }}
+</template>
+
+<style>
+
+</style>
