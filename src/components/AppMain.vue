3f185bb5cf19aa6d49941d54ca7e18e708c10517
simoneevacca <v.simone.vacca@gmail.com>
2024-04-03 17:19:29 +0200

    add film lisr

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,23 +6,21 @@ export default {
     name: 'AppMain',
     data() {
         return {
-            search: 'avatar',
-           
+            state,
+            search: 'avatar',           
             
         }
     },
 
     methods: {
         searchContent(){
-        state.GetContent(`https://api.themoviedb.org/3/search/movie?api_key=41b23e11302057b0778ead19acd303ef&query=${this.search}&include_adult=false&language=it&page=1`)
-            
+            state.GetContent(`https://api.themoviedb.org/3/search/movie?api_key=41b23e11302057b0778ead19acd303ef&query=${this.search}&include_adult=false&language=it&page=1`)
+           
         }
     },
 
-    mounted() {
-       
+    mounted() {  
     }
-
 }
 
 </script>
@@ -32,8 +30,17 @@ export default {
     <button @click="searchContent"> cerca </button>
 
 
+   
+        <div v-for="item in state.contentElement">
+            <div class="card">
+        <div>{{ item.title }}</div>
+        <div>{{ item.original_title }}</div>
+        <div>{{ item.original_language }}</div>
+        <div>{{ item.vote_average }}</div>
+    </div>
+
         
-        
+        </div>
     
     
 
--- state.js
+++ state.js
@@ -3,24 +3,27 @@ import { reactive } from 'vue'
 import axios from 'axios';
 
 export const state = reactive({
-   
+
 
     contents: [],
-   
+    contentElement: [],
+
 
     GetContent(url) {
+    this.contentElement = []
 
         axios.get(url)
             .then(response => {
                 this.contents = response.data
-                
+
                 console.log(this.contents);
-                
-            }),
-            
-            this.contents.forEach(element => {
-                console.log(element);
+                this.contents.results.forEach(element => {
+                    this.contentElement.push(element)
+                })
+                console.log(this.contentElement);
+
             })
+
 
     }
 })
